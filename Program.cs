679d73acc4bfe46399be9493eb344263d989f547
AkromAbdaliev <jacquesalaiyxs@gmail.com>
2024-11-23 09:43:22 +0200

    polymorphism demonstrated

--- Program.cs
+++ Program.cs
@@ -6,21 +6,31 @@ lion.Family = "Mammals";
 lion.Type = "Wild";
 lion.Status = "Predator";
 lion.Pride = "Western";
-lion.AnimalSound();
-lion.AnimalEats();
+// lion.AnimalSound();
+// lion.AnimalEats();
 
 var hyena = new Hyena();
 hyena.Family = "Mammals";
 hyena.Type = "Wild";
 hyena.Status = "Predator";
 hyena.Clan = "Eastern";
-hyena.AnimalSound();
-hyena.AnimalEats();
+// hyena.AnimalSound();
+// hyena.AnimalEats();
 
 var zebra = new Zebra();
 zebra.Family = "Mammals";
 zebra.Type = "Wild";
 zebra.Status = "Herbivores";
 zebra.Group = "Lakeside";
-zebra.AnimalSound();
-zebra.AnimalEats();
+// zebra.AnimalSound();
+// zebra.AnimalEats();
+
+List<Animal> animals = new List<Animal>();
+animals.Add(lion);
+animals.Add(hyena);
+animals.Add(zebra);
+
+foreach (var animal in animals)
+{
+    animal.AnimalSound();
+}
